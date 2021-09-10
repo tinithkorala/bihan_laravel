0b744ef80f8735e26937d3e0714b8fdcdafca874
zmhp <mhperera1995@gmail.com>
2021-09-10 11:37:54 +0530

    Added FB link in contact us page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -11,38 +11,48 @@
 
             <div class="card bg-dark-transparent text-white mb-5">
 
-                <div class="col-sm-12 col-md-12 col-lg-12">
+                <div class="row">
 
-                    <p class="regular-paragraph text-light">
-                        Let's get one of our travel experts to guide you through your next travel! Reach us and we'd love to help you out 
-                        with inquiries you have with a free consultation
-                    </p>
+                    <div class="col-sm-12 col-md-12 col-lg-12">
 
-                </div>
+                        <p class="regular-paragraph text-light">
+                            Let's get one of our travel experts to guide you through your next travel! Reach us and we'd love to help you out 
+                            with inquiries you have with a free consultation
+                        </p>
 
-                <div class="col-sm-12 col-md-12 col-lg-6">
-                    <h5 class="text-bold">Address</h5>
-                    <p class="small-paragraph text-light">
-                        Travel Bag (Pvt) Ltd <br>
-                        U.G 46, People's park, <br>
-                        Colombo 01100 <br>
-                        Sri Lanka
-                    </p>
-                </div>
-
-                <div class="col-sm-12 col-md-12 col-lg-6">
-
-                    <div class="row">
-                        <h5 class="text-bold">Hotline</h5>
-                        <p class="small-paragraph text-light">(+94) 114 395 800</p>
                     </div>
 
-                    <div class="row">
-                        <h5 class="text-bold">Email</h5>
-                        <p class="small-paragraph text-light">info@travelbag.biz</p>
+                    <div class="col-sm-12 col-md-12 col-lg-6">
+
+                        <h5 class="text-bold">Address</h5>
+                        <p class="small-paragraph text-light">
+                            Travel Bag (Pvt) Ltd <br>
+                            U.G 46, People's park, <br>
+                            Colombo 01100 <br>
+                            Sri Lanka
+                        </p>
+
+                        <div class="row">
+                            <h5 class="text-bold">Hotline</h5>
+                            <p class="small-paragraph text-light">(+94) 114 395 800</p>
+                        </div>
+    
+                        <div class="row">
+                            <h5 class="text-bold">Email</h5>
+                            <p class="small-paragraph text-light">info@travelbag.biz</p>
+                        </div>
+    
+                    </div>
+    
+                    <div class="col-sm-12 col-md-12 col-lg-6">
+                        <a class="decoration-none" href="https://www.facebook.com/TheTravelbagglobal/?ref=page_internal">
+                            <img src="./images/icon_facebook.svg" alt="" width="25" height="25" class="me-2">
+                            <h5 class="text-bold mb-0">Follow us on Facebook</h5>
+                        </a>
                     </div>
 
                 </div>
+                
 
             </div>
 
@@ -58,7 +68,7 @@
 
                     <div class="col-sm-12 col-md-6 col-lg-6 mb-2">
                         <label class="small-paragraph" for="">Name</label>
-                        <input type="text" name="name" id="name" class="form-control" value="{{ old('name') }}">
+                        <input type="text" name="name" id="name" class="form-control" value="{{ old('name') }}" required>
                         @error('name')
                             <p class="fw-bold text-danger">{{ $message }}</p>
                         @enderror
@@ -66,7 +76,7 @@
 
                     <div class="col-sm-12 col-md-6 col-lg-6 mb-2">
                         <label class="small-paragraph" for="">Phone</label>
-                        <input type="text" name="phone" id="phone" class="form-control" value="{{ old('phone') }}">
+                        <input type="text" name="phone" id="phone" class="form-control" value="{{ old('phone') }}" required>
                         @error('phone')
                             <p class="fw-bold text-danger">{{ $message }}</p>
                         @enderror
@@ -74,7 +84,7 @@
 
                     <div class="col-sm-12 col-md-6 col-lg-6 mb-2">
                         <label class="small-paragraph" for="">Subject</label>
-                        <input type="text" name="subject" id="subject" class="form-control" value="{{ old('subject') }}">
+                        <input type="text" name="subject" id="subject" class="form-control" value="{{ old('subject') }}" required>
                         @error('subject')
                             <p class="fw-bold text-danger">{{ $message }}</p>
                         @enderror
@@ -82,7 +92,7 @@
 
                     <div class="col-sm-12 col-md-6 col-lg-6 mb-2">
                         <label class="small-paragraph" for="">Email</label>
-                        <input type="email" name="email" id="email" class="form-control" value="{{ old('email') }}">
+                        <input type="email" name="email" id="email" class="form-control" value="{{ old('email') }}" required>
                         @error('email')
                             <p class="fw-bold text-danger">{{ $message }}</p>
                         @enderror
@@ -90,7 +100,7 @@
 
                     <div class="col-sm-12 col-md-12 col-lg-12 mb-2">
                         <label class="small-paragraph" for="">Message</label>
-                        <textarea name="messageArea" id="messageArea" cols="30" rows="6" class="form-control" >{{ old('message') }}</textarea>
+                        <textarea name="messageArea" id="messageArea" cols="30" rows="6" class="form-control" required >{{ old('message') }}</textarea>
                         @error('messageArea')
                             <p class="fw-bold text-danger">{{ $message }}</p>
                         @enderror
